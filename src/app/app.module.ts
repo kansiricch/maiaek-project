7c7915849a64515d9b621bce8d20ce83a01c38b0
ARME4N <armmps.x@gmail.com>
2020-05-30 16:51:27 +0700

    try to start for beginning Log in Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponentComponent } from './maiaek/login-component/login-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponentComponent
   ],
   imports: [
     BrowserModule,
