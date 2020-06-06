803ed66cb022d1ac3db45940f0604c0f88ddccb4
putthatida <putthatidaa@gmail.com>
2020-06-06 14:32:58 +0700

    manage bill

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponentComponent } from './maiaek/login-component/login-component.component';
+import { ManageBillComponent } from './maiaek/manage-bill/manage-bill.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponentComponent
+    LoginComponentComponent,
+    ManageBillComponent
   ],
   imports: [
     BrowserModule,
