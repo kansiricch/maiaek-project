2f583a116758d4b2811d0e0b09799410559421e7
neney <mnaiyanet.m@gmail.com>
2020-06-06 14:43:40 +0700

    add company

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponentComponent } from './maiaek/login-component/login-component.component';
 import { ManageBillComponent } from './maiaek/manage-bill/manage-bill.component';
+import { CompanyComponent } from './maiaek/company/company.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponentComponent,
-    ManageBillComponent
+    ManageBillComponent,
+    CompanyComponent
   ],
   imports: [
     BrowserModule,
